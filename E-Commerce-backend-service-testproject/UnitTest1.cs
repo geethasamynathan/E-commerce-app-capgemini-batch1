92caa37368ad73d567bc99936954993f0e3fb6af
geethasamynathan <geethasamynathan2011@gmail.com>
2024-10-12 15:08:33 +0530

    Add project files.

---
+++ E-Commerce-backend-service-testproject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace E_Commerce_backend_service_testproject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ E-Commerce-frontend-service-testproject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace E_Commerce_frontend_service_testproject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
